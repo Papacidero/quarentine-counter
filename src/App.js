1a4fb731e76afc24614775f2f800ef39b0381939
Carlos Eduardo Papacidero <papacidero@Carloss-MacBook-Air.local>
2020-04-03 14:24:19 +0200

    working with the covid_api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
 import quotes from './quotes';
@@ -9,15 +9,46 @@ function App() {
 
   const [fromDate, setFromDate] = useState({date: null, changed: false})
   const [toDate, setToDate] = useState({date: null, changed: true})
+  const [data, setdata] = useState({userCountry: '', userCountryTotalRecovered: '', worldTotalRecovered: ''});
+
+  useEffect(()=>{
+    async function loadContent() {
+      let requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+      const userCountry = await fetch('https://extreme-ip-lookup.com/json/')
+        .then( res => res.json())
+        .then(response => response.country)
+        .catch((data, status) => {
+            console.log('Request failed');
+        })
+      const data = await fetch("https://api.covid19api.com/summary", requestOptions)
+        .then(response => response.text())
+        .then(result => JSON.parse(result))
+        .catch(error => console.log('error', error));
+
+        
+      setdata({
+        userCountry,
+        userCountryTotalRecovered: data.Countries.filter(item => item.Country.match(new RegExp(`${userCountry}`,'gi')))[0].TotalRecovered,
+        worldTotalRecovered: data.Countries.reduce((prev, curr) => prev + curr.TotalRecovered, 0)
+      });
+    }
+    loadContent();
+    console.log(data)
+  }, [fromDate])
 
   const randomNumber =  ({min = 1, max = 100} = {})=> {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
   return (
-    <div className="App">
+    <div className="Quarentine">
       <div className="background_image"></div>
-      <header className="App-header">
+
+      <div className="container">
+      <content className="content">
       { !fromDate.date && !fromDate.changed &&
         <>
           <h1>Since when you are at home?</h1>
@@ -57,7 +88,7 @@ function App() {
 
       <br />
 
-      <audio controls autoPlay>
+      <audio controls>
         <source src="music.mp3" type="audio/mpeg" />
         Your browser does not support the audio element.
       </audio>
@@ -65,9 +96,19 @@ function App() {
       <br />
 
 
-      <div class="fb-share-button" data-href="https://quarentine-counter.firebaseapp.com/" data-layout="button" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fquarentine-counter.firebaseapp.com%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
+      <div className="fb-share-button" data-href="https://quarentine-counter.firebaseapp.com/" data-layout="button" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fquarentine-counter.firebaseapp.com%2F&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore">Share</a></div>
 
-      </header>
+      </content>
+      { fromDate.changed && toDate.changed && !toDate.date &&
+        <>
+        <aside>
+          <h1>A total of <span>{data.worldTotalRecovered}</span> people are already fully revovered</h1>
+          <h2>And in <span>{data.userCountry}</span> a total of <span>{data.userCountryTotalRecovered}</span></h2>
+        </aside>  
+        </>
+      }
+      </div>
+      
     </div>
   );
 }
